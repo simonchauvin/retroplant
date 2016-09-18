236a64f519d2471fd663b65993b591d74cc43105
Simon Chauvin <contact@simonchauvin.com>
2016-09-18 14:08:42 +0200

    Basic retrieval of drawings made into meshes with collider.

---
+++ Assets/Scripts/Drawing.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Drawing : MonoBehaviour
+{
+    private MeshFilter thisMeshFilter;
+    private MeshCollider thisMeshCollider;
+
+    private Vector2[] points;
+    private int type;
+
+
+	void Awake ()
+    {
+        thisMeshFilter = GetComponent<MeshFilter>();
+        thisMeshCollider = GetComponent<MeshCollider>();
+        points = new Vector2[0];
+	}
+	
+	void Update ()
+    {
+       
+	}
+
+    public void draw ()
+    {
+        thisMeshFilter.mesh = createMesh();
+        thisMeshCollider.sharedMesh = thisMeshFilter.mesh;
+
+        // Hack to have all drawings facing the camera
+        if (thisMeshFilter.mesh.normals[0].z > 0)
+        {
+            transform.Rotate(new Vector3(0f, 180f, 0f));
+        }
+
+        /*
+        thisMeshFilter.mesh = createMesh();
+        Mesh sourceMesh = new Mesh();
+        sourceMesh = thisMeshFilter.mesh;
+        Mesh workingMesh = CloneMesh(sourceMesh);
+        thisMeshFilter.mesh = workingMesh;
+
+        int iterations = 1;
+        for (int i = 0; i < iterations; i++)
+        {
+            //workingMesh.vertices = SmoothFilter.laplacianFilter(workingMesh.vertices, workingMesh.triangles);
+            workingMesh.vertices = SmoothFilter.hcFilter(thisMeshFilter.mesh.vertices, thisMeshFilter.mesh.vertices, thisMeshFilter.mesh.triangles, 0.0f, 0.5f);
+        }
+        thisMeshCollider.sharedMesh = thisMeshFilter.mesh;
+        */
+    }
+
+    private static Mesh CloneMesh(Mesh mesh)
+    {
+        Mesh clone = new Mesh();
+        clone.vertices = mesh.vertices;
+        clone.normals = mesh.normals;
+        clone.tangents = mesh.tangents;
+        clone.triangles = mesh.triangles;
+        clone.uv = mesh.uv;
+        clone.uv2 = mesh.uv2;
+        clone.bindposes = mesh.bindposes;
+        clone.boneWeights = mesh.boneWeights;
+        clone.bounds = mesh.bounds;
+        clone.colors = mesh.colors;
+        clone.name = mesh.name;
+        //TODO : Are we missing anything?
+        return clone;
+    }
+
+    public void addPoint(float x, float y)
+    {
+        Vector2[] newPoints = new Vector2[points.Length + 1];
+        for (int i = 0; i < points.Length; i++)
+        {
+            newPoints[i] = points[i];
+        }
+        newPoints[newPoints.Length - 1] = new Vector2(x, y);
+        points = newPoints;
+    }
+
+    Mesh createMesh()
+    {
+        Mesh mesh = new Mesh();
+        mesh.name = "drawing";
+
+        Vector3[] vertices = new Vector3[points.Length];
+        //int[] triangles = new int[(points.Length / 2) * 3];
+        //Vector2[] uv = new Vector2[points.Length];
+        for (int i = 0; i < points.Length; i++)
+        {
+            vertices[i] = points[i];
+
+            /*uv = new Vector2[] {
+                new Vector2 (0, 0),
+                new Vector2 (0, 1),
+                new Vector2(1, 1),
+                new Vector2 (1, 0)
+            };*/
+        }
+
+        mesh.vertices = vertices;
+        //mesh.uv = uv;
+        Triangulator tri = new Triangulator(points);
+        mesh.triangles = tri.Triangulate();
+        mesh.RecalculateNormals();
+
+        return mesh;
+    }
+
+    public Vector2[] getPoints()
+    {
+        return points;
+    }
+}
---
+++ Assets/Scripts/DrawingsManager.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class DrawingsManager : MonoBehaviour
+{
+    public string ip;
+    public int port;
+    public Vector2 resolution;
+    public Drawing drawingPrefab;
+
+    private UdpClient client;
+    private IPEndPoint ipep;
+    private string data;
+    private string[] dataDrawings;
+    private string[] dataPoints;
+    private string[] point;
+    private Drawing[] drawings;
+
+
+    void Start ()
+    {
+        ipep = new IPEndPoint(IPAddress.Parse(ip), port);
+        client = new UdpClient();
+        client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+        client.Client.Bind(ipep);
+    }
+	
+	void Update ()
+    {
+        // Retrieve data from openframeworks
+        if (client.Available > 0)
+        {
+            data = Encoding.UTF8.GetString(client.Receive(ref ipep));
+            if (data != null && data.Length > 0)
+            {
+                for (int i = 0; i < transform.childCount; i++)
+                {
+                    Destroy(transform.GetChild(i).gameObject);
+                }
+
+                dataDrawings = data.Split(';');
+                drawings = new Drawing[dataDrawings.Length];
+                for (int i = 0; i < dataDrawings.Length; i++)
+                {
+                    dataPoints = dataDrawings[i].Split(',');
+                    drawings[i] = Instantiate(drawingPrefab);
+                    drawings[i].transform.parent = transform;
+                    for (int j = 0; j < dataPoints.Length; j++)
+                    {
+                        point = dataPoints[j].Split('&');
+                        drawings[i].addPoint(float.Parse(point[0]), float.Parse(point[1]));
+                    }
+                    drawings[i].draw();
+                }
+            }
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        client.Close();
+    }
+}
