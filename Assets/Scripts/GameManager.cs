121b26f5986da9a1c6e306097e4bc9cd7ecce858
Simon Chauvin <contact@simonchauvin.com>
2016-11-13 22:22:16 +0100

    DrawingManager prefab and game manager.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    private GameManager _instance;
+
+
+    public GameManager GetInstance ()
+    {
+        if (_instance == null)
+        {
+            _instance = GameObject.FindObjectOfType<GameManager>();
+        }
+        return _instance;
+    }
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+	
+	}
+}
